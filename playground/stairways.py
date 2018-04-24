91ebf80a6199ad96b3e1c6731912e869e47fc527
Aleksey Peshekhonov <peshekhonov.a@citilink.ru>
2018-04-24 15:02:40 +0300

    stairways solution

---
+++ playground/stairways.py
@@ -0,0 +1,11 @@
+import sys
+
+num_steps = int(sys.argv[1])
+hashes = 1
+spaces = num_steps - hashes
+
+while spaces >= 0:
+  result_string = " " * spaces + "#" * hashes
+  print(result_string)
+  hashes += 1
+  spaces -= 1
